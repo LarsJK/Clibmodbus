8518dbd868081c7e9e1a0b4889ca5e392cd6d4bb
Lars-Jørgen Kristiansen <LarsJK.84@gmail.com>
2019-07-24 14:21:47 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,6 @@
-// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Clibmodbus",
-    pkgConfig: "libmodbus",
-    providers: [
-      .Brew("libmodbus"),
-      .Apt("libmodbus-dev")
-    ]
+    pkgConfig: "libmodbus"
 )
